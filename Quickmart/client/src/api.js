551b76b22868ce8cb775154aa231a4170e190220
Jasmin <jasmimulani@gmail.com>
2026-01-31 17:30:16 +0530

    "31/01/26"

--- Quickmart/client/src/api.js
+++ Quickmart/client/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // backend URL
-  withCredentials: true, // cookies ke liye
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // cookies/session ke liye
 });
 
 export default API;
